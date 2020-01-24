701dc7caab217cd72a2bb99e63fc8fb969604571
Lisa Scelli <lisa.66@hotmail.com>
2020-01-24 21:23:42 +0100

    zwischen upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+var dateTime = new Date();
+
+// def variable by importing date from Date(), then place it in the html doc via ID and convert it as a str
+
+document.getElementById("dateTime").innerHTML = dateTime.toLocaleString();
